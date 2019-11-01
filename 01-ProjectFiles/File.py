f5479c95c985cacc2f3f4f30d2e19621765f9556
Mohamed Bassel <48108775+mohamedbassel24@users.noreply.github.com>
2019-11-01 22:51:55 +0200

    RC-Part 2
    Working with Capacitor

--- 01-ProjectFiles/File.py
+++ 01-ProjectFiles/File.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 class Component:
     def __init__(self, Type, Node1, Node2, Value, InitialValue):
         self.Type = Type
@@ -15,14 +17,14 @@ def ParsingFile(FileName):
     for i in range(np.shape(FileContents)[0] - 1):  # loop for the lines before last
         FileContents[i] = FileContents[i][0:len(FileContents[i]) - 1]
     mTimeStamp = FileContents[0]
-    Dummy = FileContents[1]
+    NumberOfIterations = FileContents[1]
     FileContents = FileContents[2:]
     nComponentList = []
     for i in range(np.shape(FileContents)[0]):
         Line = FileContents[i].split()
         if Line[0] != 1 or Line[0] != -1:
             nComponentList.append(Component(Line[0], Line[1], Line[2], Line[3], Line[4]))
-    return nComponentList, mTimeStamp
+    return nComponentList, mTimeStamp, NumberOfIterations
 
 
 def initmatg(matrixG, ComponentList):
@@ -32,13 +34,13 @@ def initmatg(matrixG, ComponentList):
             node2 = int(component.Node2[1]) - 1
             if node1 < 0 or node2 < 0:  # for Ground Logic
                 mMax = max(node1, node2)
-                node1 = node2 = mMax
+                node1 = mMax
                 matrixG[node1][node1] += 1 / float(component.Value)
             else:
                 matrixG[node1][node1] += 1 / float(component.Value)
                 matrixG[node2][node2] += 1 / float(component.Value)
-                matrixG[node1][node2] = -1 / float(component.Value)  # leh msh += heya kman
-                matrixG[node2][node1] = -1 / float(component.Value)
+                matrixG[node1][node2] += -1 / float(component.Value)
+                matrixG[node2][node1] += -1 / float(component.Value)
 
 
 def initmatb(matrixb, ComponentList):
@@ -77,44 +79,87 @@ def initmate(matrixe, ComponentList):
             Index = Index + 1
 
 
-def initmati(matrixi, ComponentList):
+def initmati(matrixi, ComponentList, rTimeStamp):
     for component in ComponentList:
         if component.Type == "Isrc":
             node1 = int(component.Node1[1]) - 1
             node2 = int(component.Node2[1]) - 1
             if node1 >= 0:
-                matrixi[node1][0] = float(component.Value)
+                matrixi[node1][0] += float(component.Value)
             if node2 >= 0:
-                matrixi[node2][0] = float(component.Value)
+                matrixi[node2][0] += float(component.Value)
+        elif component.Type == "C":
+            node1 = int(component.Node1[1]) - 1
+            node2 = int(component.Node2[1]) - 1
+            if node1 >= 0:
+                matrixi[node1][0] += (float(component.Value) / rTimeStamp) * (float(component.InitialValue))
+            if node2 >= 0:
+                matrixi[node2][0] += (float(component.Value) / rTimeStamp) * (float(component.InitialValue))
 
 
-def WriteToFile(FileName, Step, Values, n, m):
+def WriteToFile(FileName, Step, List, n, m):
     f = open("RCircuit " + FileName + " output.txt", "w+")
     for i in range(n):
         mString = "V" + str(i + 1) + "\n"
-        mString += Step + " "
-        mString += str(Values[i]) + "\n"
-        f.write(mString)
+        for j in range(len(List)):
+            mString += str(float(Step) * (j + 1)) + " "
+            mString += str(List[j][i]) + "\n"
+            f.write(mString)
+            mString = ""
     for i in range(m):
         mString = "I_Vsrc" + str(i + 1) + "\n"
-        mString += Step + " "
-        mString += str(Values[i + n]) + "\n"
-        f.write(mString)
-    #  f.write("%f \n",  Values[i + n - 1])
+        for j in range(len(List)):
+            mString += str(float(Step) * (j + 1)) + " "
+            mString += str(List[j][i + n]) + "\n"
+            f.write(mString)
+            mString = ""
+
     f.close()
+
+
+def ConvertCap_Res(mList, rStep):
+    """This Function Convert each capacitor into Resistance"""
+    for mComp in mList:
+        if mComp.Type == "C":
+            mList.append(Component("R", mComp.Node1, mComp.Node2, str( float(rStep)/float(mComp.Value) ), "0"))
+
+    return mList
+
+
+def UpdateCInitalValue(mList, X):
+    """This Function Convert each capacitor into Resistance"""
+    for i in range(len(mList)):
+        if mList[i].Type == "C":
+            mLeft = int(mList[i].Node1[1]) - 1
+            mRight = int(mList[i].Node2[1]) - 1
+            pos = 0
+            Neg = 0
+            if mLeft < 0:
+                pos = 0
+            else:
+                pos = X[int(mList[i].Node1[1]) - 1]
+            if mRight < 0:
+                Neg = 0
+            else:
+                Neg = X[int(mList[i].Node2[1]) - 1]
+            mList[i].InitialValue = pos - Neg
+
+    return
 
 
 ComponentList = []
 TimeStamp = 0
 FileNumber = 0
+NumberOfIterations = 0
 while 1:
     g = input("Enter File# : ")
-    if 4 >= int(g) > 0:
+    if 8 >= int(g) > 0:
         FileNumber = g
         break
     else:
         print("Enter a Valid File#")
-ComponentList, TimeStamp = ParsingFile(FileNumber)
+
+ComponentList, TimeStamp, NumberOfIterations = ParsingFile(FileNumber)
 n = 0  # representing Number of Nodes
 m = 0  # representing Number of ID voltage Source
 
@@ -123,10 +168,9 @@ for mComponent in ComponentList:
     if mComponent.Type == "Vsrc":  # Count The No. of Voltage src
         m = m + 1
 
-
 #                                   Mat A
 # INITIALIZING the Matrices
-print(n)
+ComponentList = ConvertCap_Res(ComponentList, TimeStamp)
 G = np.zeros((n, n))  # for A resistance
 B = np.zeros((n, m))  # connection of the voltage sources
 C = np.zeros((m, n))  # Transpose of B
@@ -137,23 +181,32 @@ initmatg(G, ComponentList)
 initmatb(B, ComponentList)
 C = initmatc(B)
 D = np.zeros((m, m))
-print("MatG:\n", G)
-print("MatB:\n", B)
-print("MatC:\n", C)
-print("MatD:\n", D)
+# print("MatG:\n", G)
+# print("MatB:\n", B)
+# print("MatC:\n", C)
+# print("MatD:\n", D)
 A = IniMatA(G, B, C, D)
 #                                 Mat X(Unknown)
 V = np.zeros((n, 1))  # hold the unknown voltages at each node
 J = np.zeros((m, 1))  # holds the unknown currents through the voltage sources.
 X = np.vstack((V, J))
 #                                  Mat Z
+
+mList = []
+TimeStamp = float(TimeStamp)
+NumberOfIterations = int(NumberOfIterations)
+ActualTime = TimeStamp
 I = np.zeros((n, 1))
-E = np.zeros((m, 1))
-initmate(E, ComponentList)
-initmati(I, ComponentList)
-Z = np.vstack((I, E))
-print("Z:", Z)
-# Solving The AX=Z
-print("MatA:", A, "\nMatZ", Z)
-X = np.linalg.solve(A, Z)
-WriteToFile(FileNumber, TimeStamp, X, n, m)
+while ActualTime <= (NumberOfIterations * TimeStamp):
+    E = np.zeros((m, 1))
+    initmate(E, ComponentList)
+    I = np.zeros((n, 1))
+    initmati(I, ComponentList, TimeStamp)
+    Z = np.vstack((I, E))
+
+    X = np.linalg.solve(A, Z)
+    mList.append(X)
+    UpdateCInitalValue(ComponentList, X)
+    ActualTime += TimeStamp
+
+WriteToFile(FileNumber, TimeStamp, mList, n, m)
